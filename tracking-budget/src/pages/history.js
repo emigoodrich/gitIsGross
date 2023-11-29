bb87ce6dea725a87879ee2eeb46cad08af23f088
Emily Goodrich <emily.goodrich2751@mtec.stu.edu>
2023-11-29 09:22:33 -0700

    got everything working perfectly, so now i just have to finish my project

--- tracking-budget/src/pages/history.js
+++ tracking-budget/src/pages/history.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import OutputtingLists from './outPuttingLists'
 
 const History = () => {
+    const [list, setList] = useState([]);
+    const [num, setNum] = useState(0)
+    useEffect(() => {
+        const list = JSON.parse(localStorage.getItem('list'));
+        if (list) {
+            setList(list)
+        }
+    }, []);
+    const deletingListItem = (index) => {
+        setNum(3)
+        console.log(num)
+        //don't know but yk
+    }
     return (
         <div>
-            
+            <div id="review-titles">
+                <h1>Your history</h1>
+                <h1>big money total </h1>
+            </div>
+            <OutputtingLists list={list}  deletingListItem={deletingListItem}/>
         </div>
     )
 }
-
 export default History;
