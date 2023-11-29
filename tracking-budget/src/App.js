bf1a1a9d6f6668c04a70f305154a7c75962915b4
Emily Goodrich <emily.goodrich2751@mtec.stu.edu>
2023-11-29 09:11:58 -0700

    debugged the first little bit, turns out it renders wrong if your components aren't all completely there

--- tracking-budget/src/App.js
+++ tracking-budget/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/index';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import History from './pages/history';
+import Home from './pages/'
+import AddTransaction from './pages/addTransaction';
 import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+      <div>
+      <Navbar />
+      </div>
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+        <Route path='/review' element={<History />} />
+        <Route path='/addTransaction' element={<AddTransaction />} />
+      </Routes>
+    </Router>
     </div>
-  );
+  )
 }
-
 export default App;
